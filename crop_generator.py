5bb7791284c023a50c416a7b3d7b4778e7c78081
Robin Freeman <rob@macpro-2.local>
2021-05-13 12:03:24 +0100

    - Added <= to prevent 0 pixel images being made - Added min_height and min_width params

--- crop_generator.py
+++ crop_generator.py
@@ -1,12 +1,12 @@
 import numpy as np
 from PIL import Image
 
-def defined_crop(img, top_trim = 0, bottom_trim = 0, left_trim = 0, right_trim = 0):
+def defined_crop(img, top_trim = 0, bottom_trim = 0, left_trim = 0, right_trim = 0, min_width = 50, min_height = 50):
     # Note: image_data_format is 'channel_last'
     #assert img.shape[2] == 3
     height = img.height
     width = img.width
-    if ((width < (left_trim + right_trim)) or (height < (top_trim + bottom_trim))):
+    if ((width <= ((left_trim + right_trim) + min_width)) or (height <= ((top_trim + bottom_trim) + min_height))):
         print("Warning! Image too small to trim! Leaving image as is!")
         return(img)
     else:
